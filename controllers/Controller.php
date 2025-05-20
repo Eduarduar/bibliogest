98ee18fb1c89e54b8c30daeeec633b9e83db5304
Eduarduar <eduardoarcega1997@gmail.com>
2025-05-19 21:37:58 -0600

    feat: Refactor rutas de inicio de sesión y registro en el frontend

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -3,6 +3,18 @@
     namespace app\controllers;
 
     class Controller {
-
         public function __construct(){}
+        
+        protected function apiResponse($success, $message, $data = null, $errors = null, $code = 200)
+        {
+            http_response_code($code);
+            echo json_encode([
+                'success' => $success,
+                'message' => $message,
+                'data' => $data,
+                'errors' => $errors,
+                'code' => $code,
+            ], $code);
+        }
+    
     }
