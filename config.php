09e2514896ec08f9ddcb8099ac5230ba17b5d619
Eduarduar <eduardoarcega1997@gmail.com>
2025-05-26 11:13:34 -0600

    fix: agregar valores correspondientes en config.php

--- config.php
+++ config.php
@@ -4,14 +4,14 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__ . DS);
 
 define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'],['127.0.0.1','::1']) ? true : false );
-define('PORT', IS_LOCAL ? '80' : 'REMOTE PORT');
-define('URL', IS_LOCAL ? '127.0.0.6:'. PORT . DS : 'REMOTE URL');
+define('PORT', IS_LOCAL ? '80' : '0000');
+define('URL', IS_LOCAL ? '127.0.0.6:'. PORT . DS : 'https://bibliogest.onrender.com');
 
-define('DB_HOST', IS_LOCAL ? 'localhost' : 'REMOTE HOST');
-define('DB_PORT', IS_LOCAL ? '5432' : 'REMOTE PORT');
-define('DB_USER', IS_LOCAL ? 'postgres' : 'REMOTE USER');
-define('DB_PASS', IS_LOCAL ? '3355776Ea' : 'REMOTE PASSWORD');
-define('DB_NAME', IS_LOCAL ? 'bibliogest' : 'REMOTE DATA BASE NAME');
+define('DB_HOST', IS_LOCAL ? 'localhost' : 'dpg-d0q9bknfte5s73ajf130-a.oregon-postgres.render.com');
+define('DB_PORT', IS_LOCAL ? '5432' : '5432');
+define('DB_USER', IS_LOCAL ? 'postgres' : 'blibliogest');
+define('DB_PASS', IS_LOCAL ? '3355776Ea' : '4MOQ6CmwGKteRqAIE0ukUiONfanBNCxY');
+define('DB_NAME', IS_LOCAL ? 'bibliogest' : 'bibliogest');
 
 define('CLASSES'        , ROOT . 'classes' . DS);
 define('CLASSES_PATH'   , ROOT . '..' . DS);
